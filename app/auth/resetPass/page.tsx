d656a184fec31aa579d72e94b1c8014c391453aa
Noname01908 <adeferdi19@gmail.com>
2025-04-24 19:34:03 +0700

    perbaikan script pada folder resetPass

--- app/auth/resetPass/page.tsx
+++ app/auth/resetPass/page.tsx
@@ -1,77 +1,9 @@
-"use client"
-
-import * as z from "zod"
-import { NewPasswordSchema } from "@/Schemas"
-import React, { Suspense, useState, useTransition } from 'react'
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { CardWrapper } from "@/components/auth/CardWrapper"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { useSearchParams } from "next/navigation"
-import { newPass } from "@/actions/NewPass"
-import { Input } from "@/components/ui/input"
-import { ErrorForm } from "@/components/FormError"
-import { SuccessForm } from "@/components/FormSuccess"
-import { Button } from "@/components/ui/button"
+import React from 'react'
+import ResetPassForm from '@/components/auth/resetPassForm'
 
 const Page = () => {
-    const searchParams = useSearchParams()
-    const token = searchParams.get("token")
-    const [success, setSuccess] = useState<string | undefined>("")
-    const [error, setError] = useState<string | undefined>("")
-    const [isPending, startTransition] = useTransition()
-
-    const form = useForm<z.infer<typeof NewPasswordSchema>>({
-      resolver: zodResolver(NewPasswordSchema),
-      defaultValues: {
-        password: "",
-        confirmPass: ""
-      }
-    })
-
-    const submit = (values: z.infer<typeof NewPasswordSchema>) => {
-      setError("")
-      setSuccess("")
-
-      startTransition(() => {
-        newPass(values, token).then((data) => {
-          setSuccess(data.success)
-          setError(data.error)
-        })
-      })
-    }
   return (
-    <CardWrapper backButtonHref="/auth/login" backButtonLabel="Back To Login" headerLabel="Change Your New Password">
-      <Suspense fallback={<div className="w-full h-4 bg-slate-200 animate-pulse rounded-md"/>}>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(submit)} className="space-y-6">
-          <div className="space-y-4">
-            <FormField control={form.control} name="password" render={({field}) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input {...field} disabled={isPending} type="password"/>
-                </FormControl>
-                <FormMessage/>
-              </FormItem>
-            )}/>
-            <FormField control={form.control} name="confirmPass" render={({field}) => (
-              <FormItem>
-                <FormLabel>Confirm Password</FormLabel>
-                <FormControl>
-                  <Input {...field} disabled={isPending} type="password"/>
-                </FormControl>
-                <FormMessage/>
-              </FormItem>
-            )}/>
-          </div>
-          {error && <ErrorForm message={error}/>}
-          {success && <SuccessForm message={success}/>}
-          <Button className="w-full" type="submit" variant={"outline"}>Submit</Button>
-        </form>
-      </Form>
-      </Suspense> 
-    </CardWrapper>
+    <div><ResetPassForm/></div>
   )
 }
 
